8d3962377e074ec16751454a1645b46a5b17abdd
sashless <saweniger@gmail.com>
2015-02-24 17:39:46 +0100

    add form-decorator cache

--- gulp/index.js
+++ gulp/index.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 
 gulp = require('gulp');
+paths = {
+  'src': './src/'
+};
 
 fs.readdirSync('./gulp/tasks/').forEach(function(task) {
   require('./tasks/' + task);
--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,13 +1,19 @@
 var eslint = require('gulp-eslint'),
   jscs = require('gulp-jscs');
 
+function ignore(path) {
+  return '!' + path;
+}
+
 gulp.task('lint', ['eslint', 'jscs']);
 
 // rules set in frontend/.eslintrc
 gulp.task('eslint', function() {
   return gulp
     .src([
-      'src/**/*.js'
+      paths.src + 'modules/**/*.js',
+      paths.src + 'utility/**/*.js',
+      ignore(paths.src + 'modules/decorators/form-decorators-cache.js')
     ])
     .pipe(eslint())
     .pipe(eslint.formatEach('compact', process.stderr))
@@ -17,7 +23,9 @@ gulp.task('eslint', function() {
 gulp.task('jscs', function() {
   return gulp
     .src([
-      'src/**/*.js'
+      paths.src + 'modules/**/*.js',
+      paths.src + 'utility/**/*.js',
+      ignore(paths.src + 'modules/decorators/form-decorators-cache.js')
     ])
     .pipe(jscs('./jscs.json'));
 });
