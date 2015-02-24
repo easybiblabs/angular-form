e7d8a21f6ad023b72ea1e52aa094378fafff6c86
sashless <saweniger@gmail.com>
2015-02-24 18:40:48 +0100

    fix deps in index.js

--- src/index.js
+++ src/index.js
@@ -4,23 +4,24 @@ require('jquery');
 window.ObjectPath = require('objectpath');
 require('angular-loading-bar');
 require('angular-translate');
-require('schema-form');
+require('angular-schema-form');
 require('summernote');
 require('angular-summernote');
 require('select2');
 
-require('./../modules/sf-attrs');
-require('./../modules/datepicker');
-require('./../modules/decorators');
-require('./../modules/invalidator');
-require('./../modules/constraints');
-require('./../modules/select');
+require('./modules/sf-attrs');
+require('./modules/datepicker');
+require('./modules/decorators');
+require('./modules/invalidator');
+require('./modules/constraints');
+require('./modules/texteditor');
+require('./modules/select');
 
-require('./../utility/time');
-require('./../utility/objectpath');
-require('./../utility/summernote-lang');
+require('./utility/time');
+require('./utility/objectpath');
+require('./utility/summernote-lang');
 
-module.exports = function(){
+module.exports = (function(){
   'use strict';
 
   angular.module('angular-form', [
@@ -31,4 +32,4 @@ module.exports = function(){
     'form-decorators',
     'form-datepicker'
   ]);
-};
+})();
