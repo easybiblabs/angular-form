275ef38ae298eec6668ec4a01e7a1f29f363b352
sashless <saweniger@gmail.com>
2015-02-24 15:46:42 +0100

    update setup-karma.js

--- src/setup-karma.js
+++ src/setup-karma.js
@@ -1,7 +1,14 @@
 window.$ = window.jQuery = require('jquery');
 require('angular');
 require('angular-mocks');
+require('angular-summernote');
 
+window.ObjectPath = require('objectpath');
+require('./utility/objectpath');
+
+window.tv4 = require('tv4');
+window.moment = require('moment');
 
 var chai = require('chai');
 chai.should();
+
