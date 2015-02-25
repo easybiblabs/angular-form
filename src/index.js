b07764cd99bd250fc5aca5ed61f1ac7d12547fe6
sashless <saweniger@gmail.com>
2015-02-25 02:52:50 +0100

    remove vendors from index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-require('moment');
-require('angular');
-require('jquery');
-window.ObjectPath = require('objectpath');
-require('angular-loading-bar');
-require('angular-translate');
-require('angular-schema-form');
-require('summernote');
-require('angular-summernote');
-require('select2');
-
 require('./modules/sf-attrs');
 require('./modules/datepicker');
 require('./modules/decorators');
