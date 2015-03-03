7d1435a747a576da4751400e015bf1dfad74e37f
sashless <saweniger@gmail.com>
2015-03-03 08:55:33 +0100

    stop requiring vendors

--- src/utility/summernote-lang/index.js
+++ src/utility/summernote-lang/index.js
@@ -1,9 +1,5 @@
-require('jquery');
-
 module.exports = (function() {
   'use strict';
 
-  if (typeof $.summernote !== 'undefined' && typeof $.summernote.lang !== 'undefined') {
-    $.summernote.lang['en-US'].video.providers = '(YouTube, Vimeo, Instagram, DailyMotion)';
-  }
+  $.summernote.lang['en-US'].video.providers = '(YouTube, Vimeo, Instagram, DailyMotion)';
 })();
