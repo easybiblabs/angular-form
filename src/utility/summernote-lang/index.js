6b84c891f4b190a7d047b55e1bff305e301338cd
John Briggs <john.briggs@imagineeasy.com>
2015-03-18 13:56:13 -0500

    updated copy for video modal

--- src/utility/summernote-lang/index.js
+++ src/utility/summernote-lang/index.js
@@ -4,6 +4,7 @@ module.exports = (function() {
   'use strict';
 
   if (typeof $.summernote !== 'undefined' && typeof $.summernote.lang !== 'undefined') {
-    $.summernote.lang['en-US'].video.providers = '(YouTube, Vimeo, Instagram, DailyMotion)';
+  	$.summernote.lang['en-US'].video.url = 'Video URL';
+    $.summernote.lang['en-US'].video.providers = '(YouTube, Vimeo, Instagram, TeacherTube)';
   }
 })();
