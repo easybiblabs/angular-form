50ec5c9ec16ea8bc14c5e2743a5186f285041d07
John Briggs <john.briggs@imagineeasy.com>
2015-03-18 14:19:18 -0500

    fix for build

--- src/utility/summernote-lang/index.js
+++ src/utility/summernote-lang/index.js
@@ -4,7 +4,7 @@ module.exports = (function() {
   'use strict';
 
   if (typeof $.summernote !== 'undefined' && typeof $.summernote.lang !== 'undefined') {
-  	$.summernote.lang['en-US'].video.url = 'Video URL';
+    $.summernote.lang['en-US'].video.url = 'Video URL';
     $.summernote.lang['en-US'].video.providers = '(YouTube, Vimeo, Instagram, TeacherTube)';
   }
 })();
--- src/utility/summernote-lang/summernote-lang_spec.js
+++ src/utility/summernote-lang/summernote-lang_spec.js
@@ -6,6 +6,8 @@ describe('Summernote rich text editor video providers', function() {
 
   it('should not have Vine or Youku text', function() {
     var providers = $.summernote.lang['en-US'].video.providers;
+    var url = $.summernote.lang['en-US'].video.url;
     expect(providers).to.equal('(YouTube, Vimeo, Instagram, DailyMotion)');
+    expect(url).to.equal('Video URL');
   });
 });
