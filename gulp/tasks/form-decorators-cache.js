92bddbc82489b28941e9ae2809167e118b812920
Isam <isam@imagineeasy.com>
2015-04-23 16:26:15 -0400

    Removed typeahead-templates task from here

--- gulp/tasks/form-decorators-cache.js
+++ gulp/tasks/form-decorators-cache.js
@@ -1,30 +1,23 @@
-var templateCache = require('gulp-angular-templatecache');
-var minifyHtml = require('gulp-minify-html');
-var concat = require('gulp-concat');
+(function() {
+  'use strict';
 
-gulp.task('form-decorators-cache', function() {
-  return gulp.src(paths.src + 'modules/decorators/*.html')
-      .pipe(minifyHtml({
-        empty: true,
-        spare: true,
-        quotes: true
-      }))
-      .pipe(templateCache({
-        moduleSystem: 'browserify',
-        module: 'form-decorators',
-        root: './src/form/decorators/'
-      }))
-    .pipe(concat('form-decorators-cache.js'))
-    .pipe(gulp.dest(paths.src + 'modules/decorators/'));
-});
+  var templateCache = require('gulp-angular-templatecache');
+  var minifyHtml = require('gulp-minify-html');
+  var concat = require('gulp-concat');
 
-gulp.task('typeahead-templates', function() {
-    return gulp.src(paths.src + 'modules/typeahead/template/*.html')
-      .pipe(templateCache({
-        module: 'typeahead-templates',
-        moduleSystem: 'browserify',
-        root: 'template/typeahead',
-      }))
-      .pipe(concat('typeahead-templates.js'))
-      .pipe(gulp.dest(paths.src + 'modules/typeahead/template'));
-});
+  gulp.task('form-decorators-cache', function() {
+    return gulp.src(paths.src + 'modules/decorators/*.html')
+          .pipe(minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+          }))
+          .pipe(templateCache({
+            moduleSystem: 'browserify',
+            module: 'form-decorators',
+            root: './src/form/decorators/'
+          }))
+        .pipe(concat('form-decorators-cache.js'))
+        .pipe(gulp.dest(paths.src + 'modules/decorators/'));
+    });
+}());
