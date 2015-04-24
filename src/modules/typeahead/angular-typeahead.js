be10f27fbb1bf73727f7a76068ecc33d7aba341d
Isam <isam@imagineeasy.com>
2015-04-24 10:44:24 -0400

    wip [skip ci]

--- src/modules/typeahead/angular-typeahead.js
+++ src/modules/typeahead/angular-typeahead.js
@@ -1,24 +1,54 @@
-module.exports = function($timeout) {
+module.exports = function($timeout, $http) {
   'use strict';
 
   return {
     restrict: 'AE',
     require: 'ngModel',
+    templateUrl: 'templates/templateurl.html',
     scope: {
-      prompt: '@',
-      title: '@',
-      items: '=',
-      model: '=',
-      onSelect: '&'
+      school: '@',
+      district: '@',
+      state: '@',
+      schools: '=',
+      model: '='
     },
     link: function(scope, element, attrs, viewValue) {
+
       /* eslint-disable */
       console.debug('___scope ', scope);
       console.debug('___el ', element);
       console.debug('___attrs ', attrs);
       console.debug('___viewValue ', viewValue);
-      /* eslint-enable */
+      debugger;
 
+      scope.getSchool = function(school, district, state) {
+        debugger;
+
+        var params = {
+          school: school || '',
+          district: district || '',
+          state: state || ''
+        };
+
+        // set these in form definition or elsewhere and access in attrs param here
+        //  - $window.Config.autocomplete.registerTeacherAutocomplete
+        //  - API_CONFIG.REQUEST_HEADERS
+        var registerTeacherAutocomplete = 'http://easybibscholar.local/_register/teacher/autocomplete',
+          requestHeaders = {
+            headers: {
+              'Accept': 'application/hal+json',
+              'Content-type': 'application/json'
+            },
+            responseType: 'json'
+          };
+
+        return $http.post(registerTeacherAutocomplete, params, requestHeaders)
+          .then(function(response) {
+            return response.data;
+          });
+      };
+
+      /*
       scope.handleSelection = function(selectedItem) {
         scope.model = selectedItem;
         scope.current = 0;
@@ -39,6 +69,7 @@ module.exports = function($timeout) {
       scope.setCurrent = function(index) {
         scope.current = index;
       };
+      */
     }
   };
 };
