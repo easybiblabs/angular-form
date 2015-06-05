71f558670a52e548bb7f0d649592de22aa6745c5
Richard Wossal <richard@r-wos.org>
2015-06-05 19:02:43 +0200

    add bundle-collapser to stop it from putting out FQ paths

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,15 @@
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var collapse = require('bundle-collapser/plugin');
 
 gulp.task('build', [], function() {
   var bro = browserify('./src/index.js', {
-    standalone: 'angular-form'
+    standalone: 'angular-form',
+    fullPaths: false
   });
+  bro.plugin(collapse);
   bro.ignore('angular');
   return bro.bundle()
-    .pipe(source('index.js'))
+    .pipe(source('./index.js'))
     .pipe(gulp.dest('./dist'));
 });
- 
